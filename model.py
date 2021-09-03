fe72103997b86988e966e38e6425aef4ee3f8e32
noopy <young_ahn@yonsei.ac.kr>
2021-09-04 07:58:38 +0900

    add customized model with fc layers for different 3 classes

--- model.py
+++ model.py
@@ -1,6 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+import timm
+
+# Custom Model Template
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.model = timm.create_model('tf_efficientnet_b4', pretrained=True)
+        self.model.classifier = nn.Linear(1792, 1024)
+        self.fc1 = nn.Linear(1024, 3)
+        self.fc2 = nn.Linear(1024, 2)
+        self.fc3 = nn.Linear(1024, 3)
+    
+    def forward(self, x):
+        fc_output = self.model(x)
+        mask = self.fc1(fc_output)
+        gender = self.fc2(fc_output)
+        age = self.fc3(fc_output)
+        
+        return mask, gender, age      
 
 
 class BaseModel(nn.Module):
@@ -32,22 +52,3 @@ class BaseModel(nn.Module):
         x = self.avgpool(x)
         x = x.view(-1, 128)
         return self.fc(x)
-
-
-# Custom Model Template
-class MyModel(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
-
-    def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
